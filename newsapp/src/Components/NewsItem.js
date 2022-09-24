417250b8bb8b5dc3fa4e4127eae2c5a3ecf570c9
Krish Yadav <aryan.yadav.9889@gmail.com>
2022-09-24 20:34:44 +0530

    changed a bit css.

--- newsapp/src/Components/NewsItem.js
+++ newsapp/src/Components/NewsItem.js
@@ -11,12 +11,13 @@ export class NewsItem extends Component {
         return (
             
             <div>
-                <div className="card" style={{width: "18rem"}}>
+                <div className="card" style={{width: "18rem"}}> 
+                    {/* double "curly brackets" bcoz first one is to make it a js file and second one is for to make it an object. */}
                     <img src={imageUrl} className="card-img-top" alt="..."/>
                         <div className="card-body">
                             <h5 className="card-title">{title} ...</h5>
                             <p className="card-text">{description} ...</p>
-                            <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsURL} target="_blank" rel="noreferrer" className="btn btn-sm btn-primary">Read More</a>
                         </div>
                 </div>
             </div>
