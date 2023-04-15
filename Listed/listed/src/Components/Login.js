e1553ea21abbf871f6bfda95ac32ea67250df77a
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-15 22:14:13 +0530

    added styles and login component

---
+++ Listed/listed/src/Components/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+    <div className='flex'>
+     <div className='board'>
+        <h1>Board.</h1>
+    </div> 
+
+    <div className='login'>
+
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Login
