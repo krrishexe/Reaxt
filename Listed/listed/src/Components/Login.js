120426949142c1e40a49ad6b252437bd61e627c1
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-15 23:38:59 +0530

    added buttons and css

--- Listed/listed/src/Components/Login.js
+++ Listed/listed/src/Components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import glogo from "../Media/images/google.png"
+import alogo from "../Media/images/apple.png"
 
 function Login() {
   return (
@@ -9,7 +11,27 @@ function Login() {
     </div> 
 
     <div className='login'>
-
+       
+        <h2>Sign In</h2>
+        <h3>Sign in to your account</h3>
+        <div className='flex space'>
+            <button href='/' className='glogo'>
+                <img className='gap' src={glogo} alt="" height={"23px"} width={"23px"} /> <p className='para1'>Sign in with Google</p> 
+            </button>
+            <button className='glogo'>
+                 <img className='gap' src={alogo} alt="" height={"15px"} width={"13px"} /> <p className='para1'>Sign in with Apple</p> 
+            </button>
+        </div>
+        <div className="whitebg">
+            <form action="">
+                <label htmlFor="" >Email address</label>
+                <input type="email" value={"johndoe@gmail.com"} />
+                <label htmlFor="">Password</label>
+                <input type="password" value={"helloworld"}/>
+                
+            </form>
+        </div>
+        
     </div>
     </div>
     </>
