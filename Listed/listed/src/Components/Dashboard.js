d972897bc8d11b21df4835d7f2c73ecd86501f6a
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-18 09:56:24 +0530

    made json file and added react icons and css

--- Listed/listed/src/Components/Dashboard.js
+++ Listed/listed/src/Components/Dashboard.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { FiPieChart } from 'react-icons/fi';
-import {HiOutlineUserCircle} from 'react-icons/hi'
-import {AiOutlineSetting} from 'react-icons/ai'
-
+import { HiOutlineUserCircle } from 'react-icons/hi'
+import { AiOutlineSetting } from 'react-icons/ai'
+import "../Media/Styles/dashboard.css"
 
 
 
@@ -11,26 +11,27 @@ function Dashboard() {
 
 
   return (
-    <>
+    <div className='outer'>
       <div className='left-dash'>
         <div className='top-content'>
-          <h1>Board.</h1>
-          <div className="list-item"><FiPieChart /><h3>Dashboard</h3></div>
-          <div className="list-item"><FiPieChart /><h3>Transactions</h3></div>
-          <div className="list-item"><FiPieChart /><h3>Schedules</h3></div>
-          <div className="list-item"><HiOutlineUserCircle /><h3>Users</h3></div>
-          <div className="list-item"><AiOutlineSetting /><h3>Settings</h3></div>
+          <h1 className='dash-board'>Board.</h1>
+          <div className="list-item"><FiPieChart style={{color:'white'}} /> Dashboard </div>
+          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4>Transactions</h4></div>
+          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4>Schedules</h4></div>
+          <div className="list-item"><HiOutlineUserCircle style={{color:'white'}}/><h4>Users</h4></div>
+          <div className="list-item"><AiOutlineSetting style={{color:'white'}}/><h4>Settings</h4></div>
         </div>
 
         <div className='bottom-content'>
-
+          <h5>Help</h5>
+          <h5>Contact Us</h5>
         </div>
       </div>
 
       <div className='right-dash'>
 
       </div>
-    </>
+    </div>
   )
 }
 
