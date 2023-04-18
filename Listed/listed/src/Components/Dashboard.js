ba49d754fa0cbbe1835466aec542ed8d82c2d5d4
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-18 10:39:30 +0530

    added more css and left dash completye

--- Listed/listed/src/Components/Dashboard.js
+++ Listed/listed/src/Components/Dashboard.js
@@ -15,16 +15,16 @@ function Dashboard() {
       <div className='left-dash'>
         <div className='top-content'>
           <h1 className='dash-board'>Board.</h1>
-          <div className="list-item"><FiPieChart style={{color:'white'}} /> Dashboard </div>
-          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4>Transactions</h4></div>
-          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4>Schedules</h4></div>
-          <div className="list-item"><HiOutlineUserCircle style={{color:'white'}}/><h4>Users</h4></div>
-          <div className="list-item"><AiOutlineSetting style={{color:'white'}}/><h4>Settings</h4></div>
+          <div className="list-item"><FiPieChart style={{color:'white'}} /><h4 className='bold gap'>Dashboard</h4></div>
+          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4 className='gap'>Transactions</h4></div>
+          <div className="list-item"><FiPieChart style={{color:'white'}}/><h4 className='gap'>Schedules</h4></div>
+          <div className="list-item"><HiOutlineUserCircle style={{color:'white'}}/><h4 className='gap'>Users</h4></div>
+          <div className="list-item"><AiOutlineSetting style={{color:'white'}}/><h4 className='gap'>Settings</h4></div>
         </div>
 
         <div className='bottom-content'>
           <h5>Help</h5>
-          <h5>Contact Us</h5>
+          <h5 className='gap2'>Contact Us</h5>
         </div>
       </div>
 
