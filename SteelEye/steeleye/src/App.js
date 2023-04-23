54cd456837ee1ac5ee7a8bcd8b25e0ff4bb0770c
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-04-23 15:16:40 +0530

    added changes

---
+++ SteelEye/steeleye/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import List from './Components/List';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hellllooooo</h1>
+      <List/>
+    </div>
+  );
+}
+
+export default App;
---
+++ SteelEye/steeleye/src/Components/List.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect, memo } from 'react';
+import PropTypes from 'prop-types';
+
+// Single List Item
+const WrappedSingleListItem = ({
+  index,
+  isSelected,
+  onClickHandler,
+  text,
+}) => {
+
+  return (
+    <li
+      style={{ backgroundColor: (isSelected ? 'green' : 'red') }}
+      onClick={() => { onClickHandler(index) }}       // use of callback function
+    >
+      {text}
+    </li>
+  );
+};
+
+
+WrappedSingleListItem.propTypes = {
+  index: PropTypes.number,
+  isSelected: PropTypes.number,             // string to number
+  onClickHandler: PropTypes.func.isRequired,
+  text: PropTypes.string.isRequired,
+};
+
+const SingleListItem = memo(WrappedSingleListItem);
+
+// List Component
+const WrappedListComponent = ({
+  items,
+}) => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  useEffect(() => {
+    setSelectedIndex(null);   
+  }, [items]);
+
+  const handleClick = index => {
+    setSelectedIndex(index);
+  };
+
+  return (
+    <ul style={{ textAlign: 'left' }}>
+      {items.map((item, index) => (
+        <SingleListItem
+          onClickHandler={() => handleClick(index)}
+          text={item.text}
+          index={index}
+          isSelected={selectedIndex === index}
+          key={index}       // to provide a unique value to each list item
+        />
+      ))}
+    </ul>
+  )
+};
+
+WrappedListComponent.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+    })).isRequired,
+};
+
+WrappedListComponent.defaultProps = {
+  items: [{
+    text: "Hello"
+  },
+  {
+    text: "World"
+  },
+  {
+    text: "Welcome"
+  },
+  {
+    text: "to React"
+  }
+  ]
+};
+
+const List = memo(WrappedListComponent);
+
+export default List;
