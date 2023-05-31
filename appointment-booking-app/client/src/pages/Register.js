a56f9c5ea956fc5bbd9624d994a94aec60e76d05
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-05-31 20:59:31 +0530

    added register page and its css

--- appointment-booking-app/client/src/pages/Register.js
+++ appointment-booking-app/client/src/pages/Register.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import  {Form,Input} from 'antd'
+import "../styles/RegisterStyles.css"
+import {Link} from 'react-router-dom'
+
 
 const Register = () => {
+
+  const onFinishHandler = values => {
+    console.log('Values:', values)
+  }
+
   return (
-    <div>
-      <h1>Register</h1>
-    </div>
+    <>
+      <div className="form-container">
+        <Form layout='vertical' onFinish={onFinishHandler} className='register-form p-4 ' >
+          <h1>Register Form</h1>
+
+          <Form.Item label="Name" name="name">
+            <Input type='text'  required />
+          </Form.Item>
+
+          <Form.Item label="Email" name="email">
+            <Input type='text'  required />
+          </Form.Item>
+
+          <Form.Item label="Password" name="password">
+            <Input type='text'  required />
+          </Form.Item>
+          <div className='m-2 d-flex justify-content-center'>
+          <Link to="/login">Already a user ? Login here</Link>
+          </div>
+          <button className='btn btn-primary d-flex' type='submit'>Register</button>
+        </Form>
+      </div>
+    </>
   )
 }
 
