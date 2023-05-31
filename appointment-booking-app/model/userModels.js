c0050a74f36c70aa16d94b3c6953cb73605bf06f
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-05-31 22:12:10 +0530

    added user model and schema

--- appointment-booking-app/model/userModels.js
+++ appointment-booking-app/model/userModels.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose')
 
+//Schema
 const userSchema = new mongoose.Schema({
-    
+    name:{
+        type: String,
+        required: [true,"Name is required"]
+    },
+    email:{
+        type: String,
+        required: [true,"Email is required"]
+    },
+    password:{
+        type: String,
+        required: [true,"Password is required"]
+    },
 })
 
+//Model
 const userModel = mongoose.model('users', userSchema)
 
 module.exports = userModel
