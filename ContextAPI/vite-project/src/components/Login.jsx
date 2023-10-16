28680fdb3549a746621bf3e6918a30fe348efa7f
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-10-17 01:54:55 +0530

    updated comments

--- ContextAPI/vite-project/src/components/Login.jsx
+++ ContextAPI/vite-project/src/components/Login.jsx
@@ -18,7 +18,6 @@ function Login() {
         <div>
             <input type="text" placeholder='username' value={username} onChange={(e) => { setUsername(e.target.value) }} />
             <input type="text" placeholder='password' value={password} onChange={(e) => { setPassword(e.target.value) }} />
-
             <input type="submit" onClick={handleOnClick} value={"Submit"} />
         </div>
     )
--- ContextAPI/vite-project/src/context/UserContextProvider.jsx
+++ ContextAPI/vite-project/src/context/UserContextProvider.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import userContext from "./UserContext";
 
 const userContextProvider = ({children}) =>{
     const [user,setUser] = useState(null)
-    // variable bana denge , state me store kar lenge , fir usko provider me pass kar denge.
+    // API se jo Data ko pass karne ke liye variable bana denge , state me store kar lenge , fir usko provider me pass kar denge.
     // we can directly call api right here and pass it to the children components like , <App /> and subchildren.
     
     return (
