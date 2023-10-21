e8c7ee1340db1b7f43d99bd5b64f1e7d3edfaef1
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-10-21 13:57:46 +0530

    redux toolkit complete

--- Redux-Toolkit/vite-project/src/App.jsx
+++ Redux-Toolkit/vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
 
 function App() {
 
   return (
     <>
-      <h1>Learn Redux Toolkit</h1>
+      <AddTodo />
+      <Todo />
     </>
   )
 }
