819934675e9ceab0a9dbd274977d39ce0115593f
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-11-02 20:23:05 +0530

    added formink gignup compoonnent

--- Chatty/public/src/pages/Register.jsx
+++ Chatty/public/src/pages/Register.jsx
@@ -3,41 +3,42 @@ import logo from '../assets/logo.svg'
 import { useState } from 'react'
 import {toast,ToastContainer} from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
-
+import {useFormik} from 'formik'
 
 function Register() {
 
-  const [values,setValues] = useState({
+  const initialValues = {
     username:'',
     email:'',
     password:'',
     confirmPassword:'',
+  }
+
+  const {values ,errors,touched , handleChange , handleSubmit ,handleBlur} = useFormik({
+    initialValues:initialValues,
+    // validationSchema:"",
+    onSubmit:(values,action)=>{
+      console.log(values)
+      action.resetForm()
+    }
+
   })
-
   
-  const handleChange = (e) => {
-    setValues({...values , [e.target.name]:e.target.value});
-    console.log(values)
-  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    handleValidation()
-  }
+  console.log(values)
+  // const handleValidation = () => {
+  //   const {password,confirmPassword,email,username} = values;
 
-  const handleValidation = () => {
-    const {password,confirmPassword,email,username} = values;
-
-    if(password !== confirmPassword){
-      toast.error('Password and confirm password do not match',{
-        position:'bottom-right',
-        autoClose:8000,
-        pauseOnHover:true,
-        draggable:true,
-        theme:'dark'
-      })
-    }    
-  }
+  //   if(password !== confirmPassword){
+  //     toast.error('Password and confirm password do not match',{
+  //       position:'bottom-right',
+  //       autoClose:8000,
+  //       pauseOnHover:true,
+  //       draggable:true,
+  //       theme:'dark'
+  //     })
+  //   }    
+  // }
 
   return (
     <>
@@ -49,13 +50,13 @@ function Register() {
             <h1 className='text-white uppercase'>Chatty</h1>
           </div>
 
-          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='text' type="text" placeholder='username' onChange={handleChange} />
+          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='username' type="text" placeholder='username' value={values.username} onBlur={handleBlur} onChange={handleChange} />
 
-          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='email' type="email" placeholder='Email' onChange={handleChange} />
+          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='email' type="email" placeholder='Email' value={values.email} onBlur={handleBlur} onChange={handleChange} />
 
-          <input  className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='password' type="password" placeholder='Enter password' onChange={handleChange} />
+          <input  className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='password' type="password" placeholder='Enter password' onBlur={handleBlur} value={values.password} onChange={handleChange} />
 
-          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='confirmPassword' type="password" placeholder='Confirm password' onChange={handleChange} />
+          <input className='bg-transparent p-2 border-2 rounded-md border-blue-400 text-white w-full text-base focus:border-purple-400 focus:outline-none' name='confirmPassword' type="password" placeholder='Confirm password' value={values.confirmPassword} onBlur={handleBlur} onChange={handleChange} />
 
           <button className='text-white bg-violet-500 px-8 py-4 border-none font-bold text-lg cursor-pointer rounded-sm uppercase hover:bg-violet-400 transition-colors duration-200' type='submit'>Create User</button>
 
