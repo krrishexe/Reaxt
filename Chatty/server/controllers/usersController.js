65aeb7280b0a35f61b8f4ae4a8cecf34bb8201d5
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-11-04 12:24:24 +0530

    added loginController and login route

--- Chatty/server/controllers/usersController.js
+++ Chatty/server/controllers/usersController.js
@@ -2,9 +2,12 @@ const User = require('../models/User.Model')
 const bcrypt = require('bcrypt')
 
 
-module.exports.register = async (req, res) => {
+module.exports.register = async (req, res, next) => {
     try {
         const { username, password, email } = req.body;
+        if (username === '' || email === '' || password === '') {
+            return res.json({ msg: "Username, Password or Email are required fields", status: false })
+        }
         const usernameCheck = await User.findOne({ username })
         if (usernameCheck) {
             return res.json({ msg: "Username already exists!", status: false })
@@ -13,11 +16,31 @@ module.exports.register = async (req, res) => {
         if (emailCheck) {
             return res.json({ msg: "Email already exists!", status: false })
         }
+
         const hashedPassword = await bcrypt.hash(password, 8)
         const user = await User.create({ email, username, password: hashedPassword });
         delete user.password
-        return res.json({ status: true, user: user , msg: "Account created successfully"})
-    } catch (error) {
-        throw error
+        return res.json({ status: true, user: user, msg: "Account created successfully" })
+    } catch (ex) {
+        next(ex)
+    }
+};
+
+
+module.exports.login = async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.json({ msg: "Incorrect Username or Password", status: false })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if (!isPasswordValid) {
+            return res.json({ msg: "Incorrect Username or Password", status: false })
+        }
+        delete user.password;
+        return res.json({ status: true, user })
+    } catch (ex) {
+        next(ex)
     }
 };
