17fc09f006e3587d8a865839ea2606391bf96be9
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-11-06 00:36:12 +0530

    better way of connecting to db

--- Chatty/server/index.js
+++ Chatty/server/index.js
@@ -10,16 +10,27 @@ app.use(express.json())
 
 app.use("/api/auth",userRoutes)     // auth vale sare routes userRoutes me hai
 
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log("DB connection established")
-}).catch(err => console.log(err));
+;(async ()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL)
+        app.on('error',(err)=>{
+            console.log("Error connecting to Mongoose :" , err)
+        })
+        
+        app.listen(process.env.PORT, () => {
+            console.log(`Server live on port ${process.env.PORT}`)
+        })
+        console.log(`MongoDB Connected`);
+    } catch (error) {
+        console.log("Error: " + error)
+        throw error
+    }
+})() //IIFE
+// mongoose.connect(process.env.MONGO_URL).then(() => {
+//     console.log("DB connection established")
+// }).catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server live on port ${process.env.PORT}`)
-})
+
 
 
 // FLOW --> INDEX --> ROUTES --> CONTROLLERS --> MODELS.
