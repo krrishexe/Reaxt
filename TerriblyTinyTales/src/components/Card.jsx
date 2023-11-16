f127586f21898a220817ee03620e3915993cc4ce
Krish Yadav <aryan.yadav.9889@gmail.com>
2023-11-17 02:53:14 +0530

    fetched data from json

--- TerriblyTinyTales/src/components/Card.jsx
+++ TerriblyTinyTales/src/components/Card.jsx
@@ -1,29 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { AiOutlineLike } from "react-icons/ai";
 
-function Card() {
+function Card(props) {
+    let [cards,setCards] = useState([])
+    useEffect(()=>{
+        setCards(props.data['posts_data'])
+    },[])
     return (
         <div>
-           
-            <div className='border border-slate-200 p-6'>
+           {
+            cards.map((card,index) =>{
+                return(
+                    <div key={index} className='border border-slate-200 p-6'>
                 <div className='flex items-center justify-between'>
-                    <h2 className='text-lg font-bold '>A Changing World Order</h2>
+                    <h2 className='text-lg font-bold '>{card.title}</h2>
                     <AiOutlineLike className='text-white bg-yellow-500 rounded-full h-5 w-5' />
                 </div>
-                <p className='text-slate-600 my-8'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam quos reprehenderit quo dolore magnam sequi obcaecati atque, distinctio eaque iure nobis quisquam alias voluptas ut error ad. Sapiente, eveniet officiis.</p>
+                <p className='text-slate-600 my-8'>{card.description}</p>
 
                 <div className='flex justify-between'>
                     <div>
-                        <span className='text-cyan-300 '>musing </span><p className='inline font-bold text-slate-600'>by username</p>
+                        <span className='text-cyan-300 '>musing </span><p className='inline font-bold text-slate-600'>by {card.ig_username}</p>
                     </div>
                     <div>
-                        <p className='flex-wrap text-gray-300'>Date . 2 mins read .102 views</p>
+                        <p className='flex-wrap text-gray-300'>{card.date} . {card.reading_time} . {card.views}</p>
                     </div>
                 </div>
-
-            </div>
+            </div> 
+                )
+            })
+            
+           }
         </div>
     )
 }
 
 export default Card
+
+
+ 
--- TerriblyTinyTales/src/components/Stories.jsx
+++ TerriblyTinyTales/src/components/Stories.jsx
@@ -8,8 +8,10 @@ import { FaEye } from "react-icons/fa";
 import { IoHeartDislikeOutline } from "react-icons/io5";
 import '../App.css'
 import Card from './Card';
+import data from "../../data.json"
 
 function Stories() {
+
   return (
     <div>
       {/* Navbar */}
@@ -21,19 +23,19 @@ function Stories() {
       <div className='relative flex justify-center items-center flex-col container'>
         <img src="https://media.licdn.com/dms/image/D4D03AQEXdD5HHqzN3g/profile-displayphoto-shrink_400_400/0/1667373388482?e=1705536000&v=beta&t=-2HnbTEGp5ORczULT2PwThEqgzYprhWRXt8isdq0NHA" alt="" className='rounded-full border-2 border-gray-700 anujImg' />
         <div className='flex content items-center'>
-          <p style={{ fontFamily: 'Josefin Sans' }} className='sm:text-2xl md:text-3xl text-lg text-black font-bold'>Anuj Gosalia</p>
+          <p style={{ fontFamily: 'Josefin Sans' }} className='sm:text-2xl md:text-3xl text-lg text-black font-bold'>{data.username}</p>
           <span><MdOutlineNetworkWifi className='text-violet-600 mx-1' /></span>
           <span><IoCheckmarkDoneCircle style={{ color: '#62e5a3' }} /></span>
         </div>
 
         <div className='flex content'>
           <div className='text-gray-300'>
-            <div className='border-gray-300 rounded-lg border-2 px-2 md:px-3 btn1'>6482</div>
+            <div className='border-gray-300 rounded-lg border-2 px-2 md:px-3 btn1'>{data.followers}</div>
             <span className='text-sm md:text-base lg:text-xl '>Followers </span>
           </div>
 
           <div className='text-gray-300'>
-            <div className='border-gray-300 rounded-lg border-2 px-2 md:px-3 btn1'>245</div>
+            <div className='border-gray-300 rounded-lg border-2 px-2 md:px-3 btn1'>{data.following}</div>
             <span className='text-sm md:text-base lg:text-xl'>Following </span>
           </div>
         </div>
@@ -41,28 +43,26 @@ function Stories() {
         <section id='mid' className='flex flex-col items-start '>
           <div className='p-8 sm:my-6 md:my-8 lg:my-10'>
             <p className='sm:text-lg md:text-xl'>Co-founder & CEO at Terribly Tiny Tales</p>
-            <a href="" className='text-cyan-300'>link to ig</a>
+            <a href="" className='text-cyan-300'>{data.userIgLink}</a>
             <div className='flex items-center justify-around'>
               <IoMdStar className='text-white bg-cyan-300 rounded-full h-5 w-5' />
-              <span>125</span>
+              <span>{data.stars}</span>
               <AiOutlineLike className='text-white bg-yellow-500 rounded-full h-5 w-5' />
-              <span>12</span>
+              <span>{data.likes}</span>
               <FaEye className='text-white bg-slate-500 rounded-full h-5 w-5' />
-              <span>57.8k</span>
+              <span>{data.views}</span>
               <IoHeartDislikeOutline className='text-white bg-pink-600 rounded-full h-5 w-5' />
-              <span>26.0k</span>
-            </div>
+              <span>{data.hearts}</span>
+            </div>  
           </div>
         </section>
 
       </div>
         <section id='bottom'>
         <button className='text-blue-400 bg-white rounded-none border border-b-0  border-slate-200 '>
-                134 Posts
+                {data.posts} Posts
             </button>
-        <Card />
-        <Card />
-        <Card />
+        <Card data={data} />
         </section>  
 
 
