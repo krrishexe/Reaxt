92cce6364fd6a73c042b7d78dd57cc96d7b162e0
Krish Yadav <75037831+krrishexe@users.noreply.github.com>
2024-03-18 18:43:02 +0530

    added controllers

--- Recoil/server/controllers/user.controller.js
+++ Recoil/server/controllers/user.controller.js
@@ -1,1 +1,69 @@
 const mongoose = require('mongoose')
+const User = require('../db/index.js')
+const jwt = require('jsonwebtoken')
+
+
+const signUpController = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (user) {
+            res.status(403).json({ message: 'User already exists' });
+        } else {
+            const newUser = new User({ username, password });
+            await newUser.save();
+            const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+            res.json({ message: 'User created successfully', token });
+        }
+    } catch (error) {
+        console.log("Error signing up : " + error)
+    }
+}
+
+const loginController = async (req, res) => {
+    try {
+        const { username, password } = req.headers;
+        const user = await User.findOne({ username, password });
+        if (user) {
+            const token = jwt.sign({ username, role: 'user' }, SECRET, { expiresIn: '1h' });
+            res.json({ message: 'Logged in successfully', token });
+        } else {
+            res.status(403).json({ message: 'Invalid username or password' });
+        }
+    } catch (error) {
+        console.log("Error signing in : " + error)
+    }
+}
+
+const courses = async (req, res) => {
+    try {
+        const courses = await Course.find({ published: true });
+        res.json({ courses });
+    } catch (error) {
+        console.log("Error in courses : " + error)
+    }
+}
+
+const particularCourse = async (req, res) => {
+    try {
+        const course = await Course.findById(req.params.courseId);
+        console.log(course);
+        if (course) {
+            const user = await User.findOne({ username: req.user.username });
+            if (user) {
+                user.purchasedCourses.push(course);
+                await user.save();
+                res.json({ message: 'Course purchased successfully' });
+            } else {
+                res.status(403).json({ message: 'User not found' });
+            }
+        } else {
+            res.status(404).json({ message: 'Course not found' });
+        }
+    } catch (error) {
+        console.log("Error in particular courses : " + error)
+    }
+}
+
+
+module.exports = { signUpController, loginController, courses, particularCourse }
--- Recoil/server/routes/user.js
+++ Recoil/server/routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const { signUpController, loginController, courses,particularCourse } = require('../controllers/user.controller.js')
 const router = express.Router()
 
 
 router.route('/signup').post(signUpController)
+router.route('/login').post(loginController)
+router.route('/courses').post(authenticateJwt, courses)
+router.route('/courses/:courseId').post(authenticateJwt, particularCourse)
